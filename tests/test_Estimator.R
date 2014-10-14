b027b42ed6416fefc90ebadb3cfeaf844d3b8a68
albhasan <albhasan@gmail.com>
2014-10-14 17:50:41 +0200

    Stuff

--- tests/test_Estimator.R
+++ tests/test_Estimator.R
@@ -2,4 +2,15 @@
 library(testthat)
 library(rfields)
 
+# Test empty object
 expect_that(length(new("Estimator")), equals(0))
+
+
+
+# New object
+knownPosition
+valueSubset
+unknownPosition
+
+#estimator01 <- new("Estimator", )
+#function(.Object, knownPosition, valueSubset, unknownPosition)
